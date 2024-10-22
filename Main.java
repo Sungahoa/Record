1cb6502b257e4d5dfb29ae8bbbff7e0c81493be2
Sungahoa <157859352+Sungahoa@users.noreply.github.com>
2024-10-22 13:49:01 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Alice", 30);
+        System.out.println(person.name());  // In ra "Alice"
+        System.out.println(person.age());   // In ra 30
+    }
+}
